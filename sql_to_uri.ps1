1f118dfb01dae8016db6cef78399f760870fb5ae
krzysztofkaczorowski <krzysiek.kacz@gmail.com>
2017-08-12 15:46:41 +0200

    yd

--- sql_to_uri.ps1
+++ sql_to_uri.ps1
@@ -1,23 +1,20 @@
-$db_name=''
-$uri=''
-$path_to_mdf=''
-$path_to_ldf=''
-$query_backup="BACKUP DATABASE AdventureWorks2014   
-TO URL = 'https://qazxswedcvfrtgb.blob.core.windows.net/backupcontainer/AdventureWorks2014_ps_backup';  
+$params="db_name='AdventureWorks2014'","url='https://qazxswedcvfrtgb.blob.core.windows.net/backupcontainer/'","path_to_mdf=''","path_to_ldf=''"
+$query_backup="BACKUP DATABASE `$(db_name)
+TO URL = '`$(url)';  
 GO "
-Invoke-Sqlcmd -Query $query_backup
+Invoke-Sqlcmd -Query $query_backup -Variable $params
 
 
 $query_restore="USE [master]
 GO
-ALTER DATABASE [AdventureWorks2014] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
+ALTER DATABASE [`$(db_name)] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
 GO
 USE [master]
 GO
-EXEC master.dbo.sp_detach_db @dbname = N'AdventureWorks2014'
+EXEC master.dbo.sp_detach_db @dbname = N'`$(db_name)'
 GO
-RESTORE DATABASE AdventureWorks2014 FROM URL = 'https://qazxswedcvfrtgb.blob.core.windows.net/backupcontainer/AdventureWorks2014_ps_backup'
+RESTORE DATABASE `$(db_name) FROM URL = '`$(url)'
 with
 replace
 GO"
-Invoke-Sqlcmd -Query $query_restore 
+Invoke-Sqlcmd -Query $query_restore -Variable $params
