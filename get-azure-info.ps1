f921c706eeaed3ad84e66dc21369300778e3b1c7
krzysztofkaczorowski <krzysiek.kacz@gmail.com>
2017-08-15 14:00:05 +0000

    new

--- get-azure-info.ps1
+++ get-azure-info.ps1
@@ -1,7 +1,7 @@
 # adds an authenticated Azure account for use in the session   
 Login-AzureRmAccount
-$subscriptionName='Developer Program Benefit'
-$resourceGroupName ='test-sql'
+$subscriptionName = 'Developer Program Benefit'
+$resourceGroupName = 'test-sql'
 $locationName = 'West Europe'
 # set the tenant, subscription and environment for use in the rest of   
 Set-AzureRmContext -SubscriptionName $subscriptionName   
@@ -13,19 +13,18 @@ New-AzureRmResourceGroup -Name $resourceGroupName -Location $locationName
 New-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName -Type Standard_RAGRS -Location $locationName   
 #>
 # Get the access keys for the ARM storage account  
-$storageAccountName = (Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName).Name
+$storageAccountName = (Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName).StorageAccountName
 $accountKeys = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName  
 
 # Create a new storage account context using an ARM storage account  
-$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $accountKeys[0].Key1 
-
+$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $accountKeys[0].Value
 # Creates a new container in blob storage  
-$container = Get-AzureStorageContainer -Context $storageContext -Name "*backup*"
+$container = Get-AzureStorageContainer -Context $storageContext -Name 'backupcontainer'
 $cbc = $container.CloudBlobContainer  
 
 # Sets up a Stored Access Policy and a Shared Access Signature for the new container  
 $permissions = $cbc.GetPermissions();  
-$policyName = $policyName  
+$policyName = 'Policy1' 
 $policy = new-object 'Microsoft.WindowsAzure.Storage.Blob.SharedAccessBlobPolicy'  
 $policy.SharedAccessStartTime = $(Get-Date).ToUniversalTime().AddMinutes(-5)  
 $policy.SharedAccessExpiryTime = $(Get-Date).ToUniversalTime().AddYears(10)  
@@ -36,4 +35,17 @@ $cbc.SetPermissions($permissions);
 # Gets the Shared Access Signature for the policy  
 $policy = new-object 'Microsoft.WindowsAzure.Storage.Blob.SharedAccessBlobPolicy'  
 $sas = $cbc.GetSharedAccessSignature($policy, $policyName)  
-Write-Host 'Shared Access Signature= '$($sas.Substring(1))'' 
+$sas_token = $sas.Substring(1)
+
+$container_name = $container.Name
+$link1="https://storageAccountName.blob.core.windows.net/$container_name"
+$Params =@("link1=$link1","sas_token=$sas_token")
+$query = "IF NOT EXISTS  
+(SELECT * FROM sys.credentials   
+WHERE name = `$(link1))
+CREATE CREDENTIAL `$(link1) WITH IDENTITY = 'SHARED ACCESS SIGNATURE'  
+,SECRET = `$(sas_token));"
+Invoke-Sqlcmd -Query $query -Variable $Params -ServerInstance (HOSTNAME.EXE) -QueryTimeout 666
+
+
+Get-AzureStorageBlob -Container $cbc.Name -Context $storageContext|sort LastModified|select Name
--- sql_to_uri.ps1
+++ sql_to_uri.ps1
@@ -6,15 +6,10 @@ Invoke-Sqlcmd -Query $query_backup -Variable $params
 
 
 $query_restore="USE [master]
+ALTER DATABASE [AdventureWorks2014] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+RESTORE DATABASE [AdventureWorks2014] FROM  URL = N'https://testsqldiag606.blob.core.windows.net/backupcontainer/adventureworks2014_backup_2017_08_15_104335.bak' WITH  FILE = 1,
+NOUNLOAD,  REPLACE,  STATS = 5
+ALTER DATABASE [AdventureWorks2014] SET MULTI_USER
 GO
-ALTER DATABASE [`$(db_name)] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
-GO
-USE [master]
-GO
-EXEC master.dbo.sp_detach_db @dbname = N'`$(db_name)'
-GO
-RESTORE DATABASE `$(db_name) FROM URL = '`$(url)'
-with
-replace
-GO"
+"
 Invoke-Sqlcmd -Query $query_restore -Variable $params
