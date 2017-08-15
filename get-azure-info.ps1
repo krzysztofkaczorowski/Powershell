6343202b7020fab4f9ed24a841c878c0e363d1cf
krzysztofkaczorowski <krzysiek.kacz@gmail.com>
2017-08-15 11:13:00 +0000

    new

---
+++ get-azure-info.ps1
@@ -0,0 +1,39 @@
+# adds an authenticated Azure account for use in the session   
+Login-AzureRmAccount
+$subscriptionName='Developer Program Benefit'
+$resourceGroupName ='test-sql'
+$locationName = 'West Europe'
+# set the tenant, subscription and environment for use in the rest of   
+Set-AzureRmContext -SubscriptionName $subscriptionName   
+
+<# create a new resource group - comment out this line to use an existing resource group  
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $locationName   
+
+# Create a new ARM storage account - comment out this line to use an existing ARM storage account  
+New-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName -Type Standard_RAGRS -Location $locationName   
+#>
+# Get the access keys for the ARM storage account  
+$storageAccountName = (Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName).Name
+$accountKeys = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName  
+
+# Create a new storage account context using an ARM storage account  
+$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $accountKeys[0].Key1 
+
+# Creates a new container in blob storage  
+$container = Get-AzureStorageContainer -Context $storageContext -Name "*backup*"
+$cbc = $container.CloudBlobContainer  
+
+# Sets up a Stored Access Policy and a Shared Access Signature for the new container  
+$permissions = $cbc.GetPermissions();  
+$policyName = $policyName  
+$policy = new-object 'Microsoft.WindowsAzure.Storage.Blob.SharedAccessBlobPolicy'  
+$policy.SharedAccessStartTime = $(Get-Date).ToUniversalTime().AddMinutes(-5)  
+$policy.SharedAccessExpiryTime = $(Get-Date).ToUniversalTime().AddYears(10)  
+$policy.Permissions = "Read,Write,List,Delete"  
+$permissions.SharedAccessPolicies.Add($policyName, $policy)  
+$cbc.SetPermissions($permissions);  
+
+# Gets the Shared Access Signature for the policy  
+$policy = new-object 'Microsoft.WindowsAzure.Storage.Blob.SharedAccessBlobPolicy'  
+$sas = $cbc.GetSharedAccessSignature($policy, $policyName)  
+Write-Host 'Shared Access Signature= '$($sas.Substring(1))'' 
